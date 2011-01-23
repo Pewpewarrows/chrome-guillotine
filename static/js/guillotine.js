b2ef3cff1dadf8bc9af7972348d2e79b6d3fcaf9
Marco Chomut <marco.chomut@gmail.com>
2011-01-22 22:52:50 -0500

    Finished most of the functionality, just need to fix some deselecting and encoding bugs.

--- static/js/guillotine.js
+++ static/js/guillotine.js
@@ -10,6 +10,25 @@
             var snippets = JSON.parse(localStorage.snippets);
             snippets.push(obj);
             localStorage.snippets = JSON.stringify(snippets);
+        },
+
+        update_snippets: function() {
+            var snippets = JSON.parse(localStorage.snippets);
+            var $snippets = $('#snippets table tbody');
+
+            $snippets.html('');
+            for (var i = 0; i < snippets.length; i++) {
+                var snip = snippets[i];
+                $snippets.append('<tr id="' + i + '"><td><input class="snip" type="checkbox" /></td><td>' + snip.url  + '</td><td class="snip-text">' + snip.text  + '</td><td><a class="remove-snip" href="javascript:;">X</a></td></tr>');
+            }
+
+            if (snippets.length === 0) {
+                $('#no-snips').show();
+                $('#snippets table').hide();
+            } else {
+                $('#no-snips').hide();
+                $('#snippets table').show();
+            }
         }
     };
 	
@@ -71,6 +90,58 @@
             },
 
             main: function() {
+                $(window).bind('storage', function(e) {
+                    Guillotine.update_snippets();
+                });
+
+                Guillotine.update_snippets();
+
+                $('input.snip').live('click', function() {
+                    var snips = $('input.snip:checked');
+                    var id = $(this).closest('tr').attr('id');
+
+                    if (snips.length == 3) {
+                        $('#' + Guillotine.first_snip + ' input.snip').attr('checked', '');
+                    }
+
+                    if (Guillotine.second_snip === undefined) {
+                        Guillotine.second_snip = id;
+                    }
+
+                    Guillotine.first_snip = Guillotine.second_snip;
+                    Guillotine.second_snip = id;
+                });
+
+                $('a.remove-snip').live('click', function() {
+                    var snippets = JSON.parse(localStorage.snippets);
+                    var snip = $(this).closest('tr');
+                    var id = parseInt(snip.attr('id'), 10);
+
+                    snippets.splice(id - 1, 1);
+                    localStorage.snippets = JSON.stringify(snippets);
+
+                    snip.remove();
+
+                    Guillotine.update_snippets();
+                });
+
+                $('#diff').click(function() {
+                    var snips = $('input.snip:checked');
+
+                    if (snips.length != 2) {
+                        alert('You must have exactly 2 snippets selected to run the comparison tool.');
+
+                        return false;
+                    }
+
+                    var first_snip = $(snips[0]).closest('tr').find('td.snip-text').html();
+                    var second_snip = $(snips[1]).closest('tr').find('td.snip-text').html();
+
+                    var diff = diffString(first_snip, second_snip);
+                    $('#result').html(diff);
+
+                    return false;
+                });
             }
         }
     });
